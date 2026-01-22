22a7a5d2cc4ba7e2c65d8cc52158c051e2c50565
Gabriel <gabrieljcoutinho@gmail.com>
2026-01-22 11:11:08 -0300

    teste

---
+++ src/App.js
@@ -0,0 +1,29 @@
+
+import "./index.css"
+
+function App() {
+  return (
+    <div className="App">
+
+
+      <header className="header">
+
+      <h1>Header</h1>
+
+      </header>
+
+
+    <section className="section">
+      <h1>Section</h1>
+    </section>
+
+
+<footer className="footer">
+  <h1>Footer</h1>
+</footer>
+
+    </div>
+  );
+}
+
+export default App;
